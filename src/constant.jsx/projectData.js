4195767f1b266d5cfafc153127ace3a3f85bd360
Cyberchebs <chebemnzomblack@gmail.com>
2025-04-23 14:54:02 +0100

    done

--- src/constant.jsx/projectData.js
+++ src/constant.jsx/projectData.js
@@ -1,8 +1,12 @@
+import cloneTubeImage from "../assets/clonetube.png";
+import freshFoodsImage from "../assets/fresffoods.png";
+import restaurantImage from "../assets/restaurant.png";
+
 const projects = [
   {
     id: 1,
     name: "/youtube clone",
-    image: "/assets/clonetube.png",
+    image: cloneTubeImage,
     description:
       "This is my YouTube clone project where you can watch youtube videos in real time intergration ",
     github: "https://github.com/Cyberchebs/youtube_clone",
@@ -12,7 +16,7 @@ const projects = [
   {
     id: 2,
     name: "freshf",
-    image: "/assets/fresffoods.png",
+    image: freshFoodsImage,
     description: " An e-commerce store where you can buy fresh foods ",
     github: "https://github.com/Cyberchebs/fresh_foods",
     live: "https://freshf.netlify.app/",
@@ -21,7 +25,7 @@ const projects = [
   {
     id: 3,
     name: "gerichtfoods",
-    image: "/assets/restaurant.png",
+    image: restaurantImage,
     description:
       " A modern ui ux restaurant app  with stunning user interface ",
     github: "https://github.com/Cyberchebs/resturant_project",
